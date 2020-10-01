994f88b54a69c3d2671676b43083da9d35554d4e
danieljsharpe <daniel.j.sharpe1995@gmail.com>
2020-10-01 13:36:57 +0100

    some corrections to toy CTMC script

--- toy_ctmc.py
+++ toy_ctmc.py
@@ -1,17 +1,18 @@
+from __future__ import print_function
 import numpy as np
 from scipy import linalg
 from math import sqrt
 
-'''
+#'''
 # note: this rate matrix satisfies global, but not detailed, balance
 K = np.array([[-5.,2.0,2.0,1.0],
               [1.5,-6.,1.5,3.0],
               [3.0,4.0,-9.,2.0],
               [0.5,0.5,1.0,-2.]])
 tau = 0.05 # lag time
-'''
-
 #'''
+
+'''
 # note: this rate matrix satisfies detailed balance
 K = np.array([[-0.142479765469,  0.082084998624,    0.030197383422,  0.030197383422,  0.            ],
               [ 0.367879441171,  -0.59100960132,    0.223130160148,  0.,              0.            ],
@@ -19,9 +20,9 @@ K = np.array([[-0.142479765469,  0.082084998624,    0.030197383422,  0.030197383
               [ 0.367879441171,  0.            ,    0.018315638889, -0.38619508006,   0.            ],
               [ 0.,              0.,                0.082084998624,  0.,             -0.082084998624]])
 tau = 5. # lag time
-#'''
+'''
 
-n = np.shape(K)[0]
+n = np.shape(K)[0] # number of states
 
 evals_K, revecs_K = np.linalg.eig(K.T)
 idx = evals_K.argsort()[-1]
@@ -31,32 +32,33 @@ D = np.diag([1./pi[i] for i in range(n)])
 
 assert abs(np.sum(pi)-1.)<1.E-08
 for i in range(n): assert abs(np.dot(pi,K)[i])<1.E-08 # check global balance condition
-print "\ntransition rate matrix:\n", K
-print "\nstationary distribution:\n", pi
-print "\neigenvalues:\n", evals_K
-
+print("\ntransition rate matrix:\n",K)
+print("\nstationary distribution:\n",pi)
+print("\neigenvalues:\n",evals_K)
+# check if detailed balance condition satisfied (not necessary for fundamental matrices, Kemeny constant etc. to be valid)
 reversible=True
 for i in range(n):
     for j in range(n):
         if abs((K[i,j]*pi[i])-(K[j,i]*pi[j]))>1.E-08: reversible=False
-print "\ndetailed balance condition satisifed?", reversible
+print("\ndetailed balance condition satisifed?",reversible)
 
 Z = np.linalg.inv(np.outer(np.ones(n),pi)-K)-np.outer(np.ones(n),pi) # fundamental matrix (some freedom in choice, here gives Kemeny constant directly as trace)
 MFPT = np.zeros((n,n))
 for i in range(n):
     for j in range(n):
-        MFPT[i,j] = (1./pi[j])*(Z[j,j]-Z[i,j])
+        MFPT[i,j] = (1./pi[j])*(Z[j,j]-Z[i,j]) # note MFPT matrix for CTMC has diagonal elems equal to zero
 for i in range(n): assert abs(np.dot(pi,MFPT[i,:])-np.trace(Z))<1.E-08 # check alt definition of Kemeny constant
 assert abs((np.sum([1./abs(x) for x in evals_K[1:]]))-np.trace(Z))<1.E-08 # Kemeny constant from CTMC eigenvalues
 # check np.dot(MFPT,pi) is a left eigenvector of the transition rate matrix associated with eigenvalue equal to zero
 kp = np.dot(K,np.dot(MFPT,pi))
 for i in range(n): assert abs(kp[i])<1.E-08
-KK = np.dot(D-np.ones((n,n)),np.linalg.inv(MFPT)) # this should recover precisely the same rate matrix as the original
+# MFPT matrix from CTMC should recover precisely the same rate matrix as the original
+KK = np.dot(D-np.ones((n,n)),np.linalg.inv(MFPT))
 for kk_elem, k_elem in zip(KK.flatten(),K.flatten()): assert abs(kk_elem-k_elem)<1.E-08
 
-print "\nfundamental matrix from CTMC:\n", Z
-print "\nKemeny constant from CTMC:\n", np.trace(Z)
-print "\nMFPT matrix from CTMC:\n", MFPT
+print("\nfundamental matrix from CTMC:\n",Z)
+print("\nKemeny constant from CTMC:\n", np.trace(Z))
+print("\nMFPT matrix from CTMC:\n",MFPT)
 
 ### DISCRETE-TIME MARKOV CHAIN
 
@@ -65,31 +67,45 @@ evals_T, revecs_T = np.linalg.eig(T.T)
 idx = evals_T.argsort()[-1]
 piT = revecs_T[:,idx]/np.sum(revecs_T[:,idx])
 evals_T = evals_T[evals_T.argsort()[::-1]]
+for i in range(n): assert abs(np.dot(pi,T)[i]-pi[i])<1.E-08 # check global balance condition
 for i in range(n): assert abs(piT[i]-pi[i])<1.E-08 # check stationary distribution is identical to CTMC result
-print "\ntransition probability matrix:\n", T
+print("\ntransition probability matrix:\n",T)
 
 ZT = np.linalg.inv(np.eye(n)-T+np.outer(np.ones(n),pi)) # Kemeny and Snell's fundamental matrix
-MFPT_T = np.dot(np.eye(n)-ZT+np.dot(np.ones((n,n)),np.diag(np.diagonal(ZT))),D)
+MFPT_T = np.dot(np.eye(n)-ZT+np.dot(np.ones((n,n)),np.diag(np.diagonal(ZT))),D) # note MFPT matrix for DTMC has non-zero diagonal elems
 for i in range(n): assert abs(np.dot(pi,MFPT_T[i,:])-np.trace(ZT))<1.E-08 # check alt definition of Kemeny constant (in terms of MFPT matrix and stat distribn)
 assert abs((1.+np.sum([1./(1.-x) for x in evals_T[1:]]))-np.trace(ZT))<1.E-08 # Kemeny constant from DTMC eigenvalues
-MFPT_nodiag = MFPT_T-np.diag(np.diagonal(MFPT_T))
 # check np.dot(MFPT_T,pi) is a left eigenvector of the transition probability matrix associated with eigenvalue equal to unity
 tp1 = np.dot(MFPT_T,pi)
 tp2 = np.dot(T,np.dot(MFPT_T,pi))
 for i in range(1,n): assert abs(tp1[0]-tp1[i])<1.E-08 # all elems of tp1 (and tp2) should be equal
 for i in range(n): assert abs(tp1[i]-tp2[i])<1.E-08
 
-print "\nKemeny constant from DTMC:\n", np.trace(ZT)*tau
-print "\nMFPT matrix from DTMC:\n", MFPT_T*tau
+print("\nKemeny constant from DTMC:\n",np.trace(ZT)*tau)
+print("\nMFPT matrix from DTMC:\n", MFPT_T*tau)
 
+# recover transition rate matrix from the MFPT matrix computed from the DTMC
 KT = np.dot(D-np.ones((n,n)),np.linalg.inv((MFPT_T-np.diag(np.diagonal(MFPT_T)))*tau))
 for i in range(n): assert abs(np.sum(KT[i,:]))<1.E-08
 ''' note that there is a many-to-one mapping of CTMCs to DTMCs. Check that the recovered
     CTMC corresponds to the DTMC that also corresponds to the original CTMC. Further note: the
     various CTMCs may also correspond to different branching probability matrices '''
-TT = linalg.expm(K*tau)
-for tt_elem, t_elem in zip(TT.flatten(),T.flatten()): assert abs(tt_elem-t_elem)<1.E-08
-print "\nrecovered transition rate matrix:\n", KT
+TT = linalg.expm(KT*tau)
+for tt_elem, t_elem in zip(TT.flatten(),T.flatten()): print("\nWARNING: recovered transition matrix does not match original"); break # assert abs(tt_elem-t_elem)<1.E-08
+print("\nrecovered transition matrix:\n",TT)
+print("\nrecovered transition rate matrix:\n",KT)
+
+# recover transition probability matrix from the MFPT matrix computed from the CTMC
+pi_mfpt = np.dot((MFPT+np.diag(D))/tau,np.diag(pi))
+pi_arr = np.outer(np.ones(n),pi)
+TK = np.dot(pi_arr-pi_mfpt,np.linalg.inv(np.eye(n)-pi_mfpt))
+print("\ntransition probability matrix from CTMC MFPTs:\n",TK)
+for i in range(n): assert abs(np.sum(TK[i,:])-1.)<1.E-08 # check row-stochasticity
+#for tk_elem, t_elem in zip(TK.flatten(),T.flatten()): assert abs(tk_elem-t_elem)<1.E-08
+quit()
+
+
+# BRANCHING PROBABILITY MATRICES
 
 # note that the branching probability matrices for the different transition rate matrices are not necessarily the same
 BK = K.copy()-np.diag(np.diagonal(K)) # branching probability matrix from original transition rate matrix
@@ -97,21 +113,49 @@ BT = KT.copy()-np.diag(np.diagonal(KT)) # branching probability matrix from reco
 for i in range(n):
     BK[i,:] *= 1./-K[i,i]
     BT[i,:] *= 1./-KT[i,i]
-print "\nBranching matrix from original rate matrix:\n", BK
-print "\nBranching matrix from recovered rate matrix:\n", BT
+print("\nBranching matrix from original rate matrix:\n",BK)
+print("\nBranching matrix from recovered rate matrix:\n",BT)
 
-# recover transition probability matrix from MFPTs
-T1 = np.dot(np.outer(np.ones(n),pi)-np.dot(MFPT_T,np.diag(pi)),np.linalg.inv(np.eye(n)-np.dot(MFPT_T,np.diag(pi))))
-for i in range(n): assert abs(np.sum(T1[i,:])-1.)<1.E-08 # check row-stochasticity
-for t1_elem, t_elem in zip(T1.flatten(),T.flatten()): assert abs(t1_elem-t_elem)<1.E-08
-print "\nT1:\n", T1
-'''
-pi_arr = np.outer(np.ones(n),pi)
-DT_arr = np.dot((MFPT/tau)+D,np.diag(pi)) # could instead use MFPT_T to get back expm(K*tau). As shown, parameterise a DTMC at lag time tau
-T2 = np.eye(n)+pi_arr-np.linalg.inv(np.eye(n)-DT_arr+np.dot(pi_arr,DT_arr)) # alternative expression
-for i in range(n): assert abs(np.sum(T2[i,:])-1.)<1.E-08
-print "\nT2:\n", T2
-'''
+# COMPUTE MFPTs FROM THE ABSORBING FUNDAMENTAL MATRIX
+
+''' function to compute NxN matrix of MFPTs from transition probability matrix by considering a set of N (N-1)x(N-1) substochastic
+    reducible transition matrices. tau_vec is a vector of waiting times; for a DTMC, each entry is equal to the lag time, for a CTMC,
+    the j-th entry is the mean waiting time for the j-th node '''
+def mfpt_mtx_reduc(Pm,tau_vec):
+    n = np.shape(Pm)[0]
+    idcs = np.array([i for i in range(n)]) # keep track of indices of nodes when swapping rows and columns of transition matrix
+    MFPT_mtx = np.zeros((n,n),dtype=float)
+    for i in range(n): # cycle through target states
+        Pm_copy = Pm.copy()
+        Pm_copy[[-1,i],:] = Pm_copy[[i,-1],:] # swap i-th row with final row, the latter is considered absorbing
+        Pm_copy[:,[-1,i]] = Pm_copy[:,[i,-1]] # swap i-th col with final col, the latter is considered absorbing
+        tau_vec_copy = tau_vec.copy()
+        tau_vec_copy[[-1,i]] = tau_vec[[i,-1]] # swap elems of vector of mean waiting times
+        idcs_copy = idcs.copy()
+        idcs_copy[[-1,i]] = idcs[[i,-1]]
+        N = np.linalg.inv(np.eye(n-1)-Pm_copy[:-1,:-1])
+        MFPT_vals = np.dot(N,tau_vec_copy[:-1])
+        d=0
+        for j in range(n): # write the computed MFPTs to the array to be returned
+            if j==i:
+                d=1; continue
+            MFPT_mtx[idcs_copy[j-d],i] = MFPT_vals[j-d]
+    return MFPT_mtx
+            
+MFPT_BK = mfpt_mtx_reduc(BK,np.array([-1./x for x in np.diagonal(K)]))
+for mfpt_bk, mfpt_ctmc in zip(MFPT_BK.flatten(),MFPT.flatten()): assert abs(mfpt_bk-mfpt_ctmc)<1.E-04
+MFPT_BT = mfpt_mtx_reduc(BT,np.array([-1./x for x in np.diagonal(KT)]))
+MFPT_BT += D*tau
+for mfpt_bt, mfpt_dtmc in zip(MFPT_BT.flatten(),MFPT_T.flatten()*tau): assert abs(mfpt_bt-mfpt_dtmc)<1.E-04
+
+ZBK = np.linalg.inv(np.eye(n)-BK+np.outer(np.ones(n),pi)) # fundamental matrix from branching probability matrix
+
+MFPT_Tr = mfpt_mtx_reduc(T,np.array([tau for i in range(n)]))
+MFPT_Tr += D*tau
+for mfpt_tr, mfpt_dtmc in zip(MFPT_Tr.flatten(),MFPT_T.flatten()*tau): assert abs(mfpt_tr-mfpt_dtmc)<1.E-04
+
+
+# QUANTITIES PERTAINING TO DETAILED BALANCE CONDITION
 
 dS = np.zeros((n,n),dtype=float) # matrix of entropy flow along edges (as multiple of Boltzmann constant)
 for i in range(n):
@@ -119,7 +163,7 @@ for i in range(n):
         if K[i,j]==0. and K[j,i]==0.: continue # nodes are not connected in either direction
         dS[i,j] = -np.log(K[i,j]/K[j,i])
         dS[j,i] = -np.log(K[j,i]/K[i,j])
-print "\nentropy flow matrix:\n", dS
+print("\nentropy flow matrix:\n",dS)
 
 if not reversible: quit()
 ''' define the symmetrized rate matrix. Note that the symmetrized form does not have rows that sum to zero;
@@ -138,4 +182,4 @@ for i in range(n): assert abs((1.*sqrt(pi[i]))-evecs_sym[0,i])<1.E-08 # test rel
 for i in range(n): # eigenvectors of symmetrised rate matrix are orthonormal
     for j in range(n):
         assert abs(np.dot(evecs_sym[i,:],evecs_sym[j,:])-(lambda i,j: 1. if i==j else 0.)(i,j))<1.E-08
-print "\neigenvectors of symmetrised rate matrix:\n", evecs_sym
+print("\neigenvectors of symmetrised rate matrix:\n",evecs_sym)
--- toy_ktn.py
+++ toy_ktn.py
@@ -1,10 +1,13 @@
 import numpy as np
 from scipy.linalg import expm
+from sys import argv
 
 '''
 A script to construct a toy kinetic transition network where the transition rates are given by an Arrhenius-type law (cf. harmonic
 transition state theory). Such a Markov chain is guaranteed to satisfy the detailed balance condition (i.e. is reversible)
 '''
+
+write_output = int(argv[1]) # write output 1/0 (Y/N)
 
 E = np.array([1.5,3.0,2.5,4.0,5.5]) # energies of discrete states
 Eb = np.array([[0.0,4.0,5.0,5.0,0.0], # off-diagonal upper-triangular elements are energies of transition states (TSs)
@@ -13,7 +16,7 @@ Eb = np.array([[0.0,4.0,5.0,5.0,0.0], # off-diagonal upper-triangular elements a
                [0.0,0.0,0.0,0.0,0.0],
                [0.0,0.0,0.0,0.0,0.0]])
 
-T = 1. # effective temperature
+beta = 1. # effective inverse temperature
 tau = 5. # lag time at which transition probability matrix is estimated
 
 n = np.shape(E)[0]
@@ -23,9 +26,10 @@ for i in range(n):
     for j in range(i+1,n):
         if Eb[i,j]==0.: continue # zero entry in Eb indicates no connection between pair of discrete states
         assert np.array([Eb[i,j]-E[i]>0.,Eb[i,j]-E[j]>0.]).all() # TS energy must exceed energies of discrete states that it connects
-        K[i,j] = np.exp((E[i]-Eb[i,j])/T)
-        K[j,i] = np.exp((E[j]-Eb[i,j])/T)
+        K[i,j] = np.exp((E[i]-Eb[i,j])*beta)
+        K[j,i] = np.exp((E[j]-Eb[i,j])*beta)
 for i in range(n): K[i,i] = -np.sum(K[i,:]) # ensures that *global* balance is satisfied (i.e. stationary distribution exists)
+T = expm(K*tau)
 
 # check that *detailed* balance condition holds
 evals, revecs = np.linalg.eig(K.T)
@@ -38,4 +42,34 @@ for i in range(n):
 
 np.set_printoptions(precision=12)
 print "\ntransition rate matrix:\n", K
-print "\ntransition probability matrix:\n", expm(K*tau)
+print "\ntransition probability matrix:\n", T
+
+if not write_output: quit()
+evals_T, revecs_T = np.linalg.eig(T.T) # eigenvalues and right eigenvectors
+idx = evals_T.argsort()[-1]
+pi = revecs_T[:,idx]/np.sum(revecs_T[:,idx]) # stationary distribution
+for i in range(n): assert abs(np.dot(pi,K)[i])<1.E-08
+for i in range(n): assert abs(np.dot(pi,T)[i]-pi[i])<1.E-08
+
+# print DISCOTRESS input files for CTMC and DTMC
+ts_wts_ctmc = open("ts_weights_ctmc.dat","w")
+ts_conns_ctmc = open("ts_conns_ctmc.dat","w")
+ts_wts_dtmc = open("ts_weights_dtmc.dat","w")
+ts_conns_dtmc = open("ts_conns_dtmc.dat","w")
+stat_prob_f = open("stat_prob.dat","w")
+for i in range(n):
+    stat_prob_f.write("%1.15f\n" % np.log(pi[i]))
+    for j in range(i+1,n):
+        if K[i,j]!=0.:
+            ts_wts_ctmc.write("%1.15f\n" % np.log(K[i,j]))
+            ts_wts_ctmc.write("%1.15f\n" % np.log(K[j,i]))
+            ts_conns_ctmc.write("%4i %4i\n" % (i+1,j+1))
+        if T[i,j]!=0.:
+            ts_wts_dtmc.write("%1.15f\n" % T[i,j])
+            ts_wts_dtmc.write("%1.15f\n" % T[j,i])
+            ts_conns_dtmc.write("%4i %4i\n" % (i+1,j+1))
+ts_wts_ctmc.close()
+ts_conns_ctmc.close()
+ts_wts_dtmc.close()
+ts_conns_dtmc.close()
+stat_prob_f.close()
