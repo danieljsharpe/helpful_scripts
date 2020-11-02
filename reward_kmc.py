50ce98e8ffede0198be67dbf0f5c399c275da211
danieljsharpe <daniel.j.sharpe1995@gmail.com>
2020-11-02 16:14:36 +0000

    started GT reward script

--- reward_kmc.py
+++ reward_kmc.py
@@ -5,6 +5,7 @@ Python script to perform kMC for a given DTMC with associated reward values for 
 from __future__ import print_function
 import numpy as np
 
+'''
 # transition probability matrix
 T = np.array([[0.50, 0.20, 0.15, 0.15, 0.00],
               [0.15, 0.75, 0.10, 0.00, 0.00],
@@ -18,11 +19,35 @@ R =  np.array([[ 0.0000000000, -0.2876820725,  0.2876820725, -1.098612289,  0.00
                [-0.2876820725,  0.0000000000,  0.0000000000,  0.916290732, -0.4054651081],
                [ 1.0986122890,  0.0000000000, -0.9162907319,  0.000000000,  0.0000000000],
                [ 0.0000000000,  0.0000000000,  0.4054651081,  0.000000000,  0.0000000000]])
+'''
+
+'''
+# renormalised network after eliminating node 1 of original chain
+T = np.array([[0.810, 0.145, 0.045, 0.000],
+              [0.180, 0.510, 0.160, 0.150],
+              [0.020, 0.265, 0.715, 0.000],
+              [0.000, 0.100, 0.000, 0.900]])
+
+R = np.array([[ 0.0000000000,  0.178561284 , -0.8109302165,  0.0000000000],
+              [-0.2557174   ,  0.0000000000,  0.05282132  , -0.4054651081],
+              [ 0.8109302165, -0.785955730 ,  0.0000000000,  0.0000000000],
+              [ 0.0000000000,  0.4054651081,  0.0000000000,  0.0000000000]])
+'''
+
+# renormalised network after eliminating node 2 of original chain
+T = np.array([[0.64736842105, 0.2026315789, 0.15],
+              [0.28026315789, 0.7197368421, 0.00],
+              [0.10         , 0.00        , 0.90]])
+
+R = np.array([[-0.01637209  , -0.18270332, -0.4054651081],
+              [-0.68926471  ,  0.00000000,  0.0000000000],
+              [ 0.4054651081,  0.00000000,  0.0000000000]])
+
 
 seed=19 # seed for random number generator
 
-b=4 # ID of initial node (indexed from 1)
-a=5 # ID of target (absorbing) node
+b=2 # ID of initial node (indexed from 1)
+a=3 # ID of target (absorbing) node
 
 npaths = 100000 # no. of paths to simulate
 
